ce4bc71b8cb24fa3c6a9afa4597f1a60dd217a74
Ram Prass <ram@catsec.com>
2025-01-01 18:36:01 +0200

    release.sh

---
+++ release.sh
@@ -0,0 +1,10 @@
+gh release create v1.0.0 \
+scrambled-pie.img.zip scrambled-pie.img.zip.pgp \
+scrambler-linux-arm64 scrambler-linux-arm64.pgp \
+scrambler-linux-x86_64 scrambler-linux-x86_64.pgp \
+scrambler-macos-intel scrambler-macos-intel.pgp \
+scrambler-macos-Mx scrambler-macos-Mx.pgp \
+scrambler-windows-x86_64 scrambler-windows-x86_64.pgp \
+--repo catsec/scrambler \
+--title "Release v1.0.0" \
+--notes "Initial release with binaries for Linux, macOS, and Windows and a disk image for Raspberry Pi 5" 
